8247305464d557102aa08efe32bbe07e8bdec72f
SeferAlen <sefer.alen@yahoo.com>
2019-12-16 19:41:05 +0100

    Heroku: Fix server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/billy'));
+app.use(express.static(__dirname + '/dist/Billy'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/billy/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Billy/index.html'));
 });
 
 // default Heroku PORT
