ed3d5a09160e5316f9830789bc1497bc3fd3b44c
SeferAlen <sefer.alen@yahoo.com>
2019-12-16 18:29:21 +0100

    Login: Change server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/'));
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
